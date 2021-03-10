48e9ebdeca6952834fe0d196d50cec2d2f740123
Johan <johan.kellen@linkoping.se>
2021-03-10 16:19:08 +0100

    errorhantering via php

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
 // detta måste tas bort innan man går live på riktigt
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
-if($_GET["fel"]){ echo "fel"; }
+
 
 //konstanter för sökvägar
 define('ROOT', dirname(__DIR__));
