727ac0eea98f10159f8c1807fbc4e19487719989
Johan <johan.kellen@linkoping.se>
2021-03-10 16:57:24 +0100

    postgres2

--- app/Database.php
+++ app/Database.php
@@ -15,14 +15,14 @@ class Database
     {
         $db = parse_url(getenv("DATABASE_URL"));
         if (!empty($db["host"])) {
-            $pdo = new \PDO("pgsql:" . sprintf(
+            $dsn = "pgsql:" . sprintf(
                     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
                     $db["host"],
                     $db["port"],
                     $db["user"],
                     $db["pass"],
                     ltrim($db["path"], "/")
-                ));
+                );
         } else {
             $dsn = "pgsql:host=localhost;port=5432;dbname=todo-demo;user=postgres;password=postgres";
         }
