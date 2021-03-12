ec5219112b477a002e904a2767057bf640ff3d59
Johan <johan.kellen@linkoping.se>
2021-03-12 09:21:25 +0100

    sätter på error report i php

---
+++ app/debug.php
@@ -0,0 +1,5 @@
+<?php
+// SÄTT PÅ ERRORRAPPORTERING MED PHP
+// detta MÅSTE tas bort innan man går live på riktigt
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
--- public/index.php
+++ public/index.php
@@ -1,16 +1,17 @@
 <?php
 
 // SÄTT PÅ ERRORRAPPORTERING MED PHP
-// detta måste tas bort innan man går live på riktigt
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-
-
+// detta MÅSTE tas bort innan man går live på riktigt
+//error_reporting(E_ALL);
+//ini_set("display_errors", 1);
 
 //konstanter för sökvägar
 define('ROOT', dirname(__DIR__));
 define('APP', ROOT . DIRECTORY_SEPARATOR . 'app');
 define('PUB', ROOT . DIRECTORY_SEPARATOR . 'public');
+
+// läs in debugfilen ifall det behövs
+require_once APP . "/debug.php";
 
 // läs in alla tillägg i vendor
 require_once ROOT . "/vendor/autoload.php";
