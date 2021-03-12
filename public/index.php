6d8d46e8d1d6cd9a129bcdb0c462bc997c313e26
Johan <johan.kellen@linkoping.se>
2021-03-12 09:02:52 +0100

    sätter på error report i php

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 // SÄTT PÅ ERRORRAPPORTERING MED PHP
 // detta måste tas bort innan man går live på riktigt
-//error_reporting(E_ALL);
+error_reporting(E_ALL);
 //ini_set("display_errors", 1);
 
 
